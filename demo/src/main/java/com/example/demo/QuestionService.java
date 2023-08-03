5a676623a8d5903eaf68062e310e7d55cc2352f5
younna <nyc6709@gmail.com>
2023-08-03 18:23:33 +0900

    youn수정

--- demo/src/main/java/com/example/demo/QuestionService.java
+++ demo/src/main/java/com/example/demo/QuestionService.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class QuestionService {
+public class QuestionService2 {
 
     private final QuestionRepository questionRepository;
 
