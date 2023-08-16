37ae530dc2edeb6d91c697eb1df60d3751a1f7ff
suwonhansibak <wndud562@gmail.com>
2023-08-16 10:01:19 +0900

    Feat : cors 해결

--- dev.wonfit/src/main/java/com/wonfit/dev/wonfit/config/CorsConfig.java
+++ dev.wonfit/src/main/java/com/wonfit/dev/wonfit/config/CorsConfig.java
@@ -1,14 +1,17 @@
 package com.wonfit.dev.wonfit.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
 
 public class CorsConfig implements WebMvcConfigurer {
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://13.209.29.104:3000/") // 허용할 출처
+                .allowedOrigins("http://localhost:3000") // 허용할 출처
                 .allowedMethods("GET", "POST","PUT") // 허용할 HTTP method
                 .allowCredentials(true) // 쿠키 인증 요청 허용
                 .maxAge(3000);
