f30e780eccb1afc8e5e3a6b6485373ea4a4eaba6
suwonhansibak <wndud562@gmail.com>
2023-08-17 09:07:29 +0900

    Feat: product entity 작성

--- dev.wonfit/src/main/java/com/wonfit/dev/wonfit/product/product/domain/Product.java
+++ dev.wonfit/src/main/java/com/wonfit/dev/wonfit/product/product/domain/Product.java
@@ -1,12 +1,10 @@
 package com.wonfit.dev.wonfit.product.product.domain;
 
+import com.wonfit.dev.wonfit.member.memberliked.domain.MemberLiked;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,4 +13,25 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name ="product_name")
+    private String productName;
+
+    @Column(name ="interest_rate")
+    private double interestRate;
+
+    @Column(name ="attribute")
+    private String attribute;
+
+    @Column(name ="sorting_options")
+    private String sortingOptions;
+
+    @Column(name ="sub_product_info")
+    private String subProductInfo;
+
+    @ManyToOne
+    @JoinColumn(name = "memberLiked_Id")
+    private MemberLiked memberLiked;
+
+
 }
